afe01b82f6d3e9deec6df527f0b56a19cf4dcadd
KarimMohamed6 <km6777804@gmail.com>
2024-03-17 16:34:46 +0200

    setup database config

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot(), DatabaseModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
