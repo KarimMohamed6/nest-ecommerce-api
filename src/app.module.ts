c23c9fe6f8baee4ae6cd75aacb65697fea0f636c
KarimMohamed6 <km6777804@gmail.com>
2024-03-17 17:53:08 +0200

    generate user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule],
   controllers: [],
   providers: [],
 })
